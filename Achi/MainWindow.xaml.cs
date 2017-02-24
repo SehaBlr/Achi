bbb49c52c7cb770a2d7180f542bc194f77207bb5
SEHAPC\Sergey <markov_ss8@mail.ru>
2017-02-25 02:06:37 +0300

    пасринг методов

--- Achi/MainWindow.xaml.cs
+++ Achi/MainWindow.xaml.cs
@@ -26,6 +26,8 @@ namespace Achi
     {
         const string apikey = "D5D66847FA5F20AFE3BA4A9280502A13";
         const string baseurlsteam = "api.steampowered.com";
+        const string dbconn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\DEV\C#\Achi\Achi\achidb.mdf;Integrated Security=True;Connect Timeout=30";
+        int j;
         public MainWindow()
         {
             InitializeComponent();
@@ -69,20 +71,20 @@ namespace Achi
             string elements = htmlContent;
             textBox.Text = htmlContent;
             ibeg = 0;
-            int i = 1;
             cleandb();
-            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\DEV\C#\Achi\Achi\achidb.mdf;Integrated Security=True;Connect Timeout=30");
+            j = 1;
+            int i = 1;
+            SqlConnection con = new SqlConnection(dbconn);
             con.Open();
             do
             {
                 iend = elements.Substring(1).IndexOf(findbeg) + 1;
-                if (iend < 30) { break; }
+                if (iend < 30) { iend = elements.Length; ; }
                 string elemdiv = elements.Substring(ibeg, iend - ibeg).Trim();
                 int len = elements.Length - iend;
                 if (len > 0)
                 {
                     elements = elements.Substring(iend, len).Trim();
-                    listBox.Text = elements;
                 }
                 else { elements = ""; }
                 ibeg = elements.IndexOf(findbeg);
@@ -99,34 +101,70 @@ namespace Achi
         {
             int begh3 = elemdiv.IndexOf("<h3>")+4;
             int endh3 = 0;
+            
             if (begh3>4)
             {
                 endh3 = elemdiv.IndexOf("</h3>");
                 string interfacename = elemdiv.Substring(begh3, endh3 - begh3);
-                SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\DEV\C#\Achi\Achi\achidb.mdf;Integrated Security=True;Connect Timeout=30");
+                SqlConnection con = new SqlConnection(dbconn);
                 con.Open();
                 SqlCommand cmd = new SqlCommand("insert into Interface (id,interface) values (@id,@interface)", con);
                 cmd.Parameters.AddWithValue("@id", i);
                 cmd.Parameters.AddWithValue("@interface", interfacename);
                 cmd.ExecuteNonQuery();
                 con.Close();
+                /*-----далее делаю парисинг методов и версий
+                 * ----elemdiv переписываю в parsdiv для вырезания фрагметов
+                */
+                string parsdiv = elemdiv;
+                string findbeg = "<div class=\"panel-heading\">";
+                int begmth = parsdiv.IndexOf(findbeg);
+                parsdiv = parsdiv.Substring(begmth);
+                begmth = 0;
+                do {
+                    int endmth = parsdiv.Substring(1).IndexOf(findbeg) + 1;
+                    if (endmth < 30) {endmth=parsdiv.Length; }
+                    string methoddiv = parsdiv.Substring(begmth, endmth - begmth).Trim();
+                    int len = parsdiv.Length - endmth;
+                    if (len > 0)
+                    {
+                        parsdiv = parsdiv.Substring(endmth, len).Trim();
+                        listBox.Text = parsdiv;
+                    } else { parsdiv = ""; }
+                    /*Здесь будет парсер метода с версией*/
+                    int ibeg = methoddiv.IndexOf("/"+ interfacename+"/")+2+interfacename.Length;
+                    int iend = methoddiv.IndexOf("</pre>");
+                    string methodname = methoddiv.Substring(ibeg, iend - ibeg);
+                    con.Open();
+                    cmd = new SqlCommand("insert into method (id,method,interfaceid) values (@id,@method,@interface)", con);
+                    cmd.Parameters.AddWithValue("@id", j);
+                    cmd.Parameters.AddWithValue("@method", methodname);
+                    cmd.Parameters.AddWithValue("@interface", i);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    j++;
+                    // конец парсера метода с версией
+                    begmth = parsdiv.IndexOf(findbeg);
+                } while (begmth != -1);
             }
         }
 
         private void cleandb()
         {
-            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\DEV\C#\Achi\Achi\achidb.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlConnection con = new SqlConnection(dbconn);
             con.Open();
             SqlCommand cmd1 = new SqlCommand("delete from testres", con);
             SqlCommand cmd2 = new SqlCommand("delete from Interface", con);
+            SqlCommand cmd3 = new SqlCommand("delete from Method", con);
             cmd1.ExecuteNonQuery();
             cmd2.ExecuteNonQuery();
+            cmd3.ExecuteNonQuery();
             con.Close();
         }
         private void show_categories()
         {
             interfacename.Items.Clear();
-            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\DEV\C#\Achi\Achi\achidb.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlConnection con = new SqlConnection(dbconn);
             try
             {
                 con.Open();
@@ -140,7 +178,6 @@ namespace Achi
                 SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM interface", con);
                 DataSet ds = new DataSet();
                 da.Fill(ds, "interface");
-
                 interfacename.ItemsSource = ds.Tables[0].DefaultView;
                 interfacename.DisplayMemberPath = ds.Tables[0].Columns["interface"].ToString();
                 interfacename.SelectedValuePath = ds.Tables[0].Columns["id"].ToString();
